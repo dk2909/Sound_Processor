06f0971eb427374c0edcd2aa5ffed81d2c04f50a
dk2909 <d.king2909@gmail.com>
2021-04-28 21:27:06 -0500

    added fft function

---
+++ Lab4/Lab4_Fitness_4C123/Objects/cr4_fft_1024_stm32_2.d
@@ -0,0 +1,1 @@
+.\objects\cr4_fft_1024_stm32_2.o: cr4_fft_1024_stm32.s
---
+++ Lab4/Lab4_Fitness_4C123/Objects/os.d
@@ -0,0 +1,14 @@
+.\objects\os.o: os.c
+.\objects\os.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\os.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\os.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\os.o: os.h
+.\objects\os.o: ../inc/CortexM.h
+.\objects\os.o: ../inc/BSP.h
+.\objects\os.o: ../inc/tm4c123gh6pm.h
+.\objects\os.o: C:\Users\dking\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\DSP\Include\arm_math.h
+.\objects\os.o: C:\Users\dking\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\os.o: C:\Users\dking\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\os.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\os.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\float.h
+.\objects\os.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\limits.h
