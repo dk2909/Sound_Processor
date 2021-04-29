21acef0070d9ba4edaa400df609c48c2c32b2432
dk2909 <d.king2909@gmail.com>
2021-04-29 17:53:59 -0500

    removed initial folder

--- Lab4/Lab4_Fitness_4C123/Objects/cr4_fft_1024_stm32_2.d
+++
@@ -1,1 +0,0 @@
-.\objects\cr4_fft_1024_stm32_2.o: cr4_fft_1024_stm32.s
--- Lab4/Lab4_Fitness_4C123/Objects/lab4.d
+++
@@ -1,7 +0,0 @@
-.\objects\lab4.o: Lab4.c
-.\objects\lab4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\lab4.o: ../inc/BSP.h
-.\objects\lab4.o: ../inc/Profile.h
-.\objects\lab4.o: Texas.h
-.\objects\lab4.o: ../inc/CortexM.h
-.\objects\lab4.o: os.h
